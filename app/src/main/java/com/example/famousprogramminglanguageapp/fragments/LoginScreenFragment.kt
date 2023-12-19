4e5c832861a145e6408efac12c951e128203a4c9
Totty <kaqiu96@gmail.com>
2023-12-19 14:06:42 +0100

    commit

---
+++ app/src/main/java/com/example/famousprogramminglanguageapp/fragments/LoginScreenFragment.kt
@@ -0,0 +1,23 @@
+package com.example.famousprogramminglanguageapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.famousprogramminglanguageapp.R
+
+
+class LoginScreenFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_screen, container, false)
+    }
+
+
+}
